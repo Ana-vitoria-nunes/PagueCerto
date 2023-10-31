8a9a8ca8c69da0b9a639417fa0a0b7931bda9748
Ana Flavia Bernardes <130581085+AnaBern034@users.noreply.github.com>
2023-10-31 11:43:03 -0300

    Update Pass.java

--- src/main/java/org/example/adapters/config/Pass.java
+++ src/main/java/org/example/adapters/config/Pass.java
@@ -4,8 +4,6 @@ import org.mindrot.jbcrypt.BCrypt;
 public class Pass {
     public static String hashCrypto(String plainTextPassword) {
         return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
-
-
     }
 
 }
